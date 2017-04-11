fb0d0b223b6b717d150a6178566d09568cdb9840
luoqingbin <361503082@qq.com>
2017-04-11 15:59:18 +0800

    修改<base href="${basePath}">

--- admin/business/src/main/java/com/badminton/admin/controller/LoginController.java
+++ admin/business/src/main/java/com/badminton/admin/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.badminton.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 登录
  */
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping(value = "doLogin")
-    public String doLogin(){
+    public String doLogin(HttpServletRequest request){
+        String path =request.getContextPath();//项目名称
+        String basePath =request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+        request.getSession().setAttribute("basePath",basePath);
         return "login";
     }
 
