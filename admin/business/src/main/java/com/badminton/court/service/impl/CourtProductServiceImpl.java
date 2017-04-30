4e0a4dd2b324059b37581d4f81d1337498cc422c
LUOQB\luoqb <aa2@qq.com>
2017-04-30 20:05:52 +0800

    修改pom

--- admin/business/src/main/java/com/badminton/court/service/impl/CourtProductServiceImpl.java
+++ admin/business/src/main/java/com/badminton/court/service/impl/CourtProductServiceImpl.java
@@ -13,7 +13,8 @@ import com.badminton.interceptors.mySqlHelper.conditionHelper.query.OrderConditi
 import com.badminton.interceptors.mySqlHelper.pagehelper.util.StringUtil;
 import com.badminton.utils.DateUtil;
 import com.badminton.utils.TimestampPkGenerator;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,7 +28,7 @@ import java.util.List;
  */
 @Service("courtProductService")
 public class CourtProductServiceImpl implements CourtProductService {
-    private Logger logger = Logger.getLogger(getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private CourtProductMapper courtProductMapper;
     @Autowired
