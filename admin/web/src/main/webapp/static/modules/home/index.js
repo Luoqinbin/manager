1f60ed3c4a9f4b845a7ec2dec91511cd4aa95d0f
luoqingbin <361503082@qq.com>
2017-05-03 17:03:08 +0800

    首页预定场地完成

--- admin/web/src/main/webapp/static/modules/home/index.js
+++ admin/web/src/main/webapp/static/modules/home/index.js
@@ -40,7 +40,36 @@ define([
                     }
                 });
                 module.initTable(area,$(this).val());
-            })
+            });
+
+            $("#fixOrder").bind("click",function () {
+                var courtId = "";
+                var startTime = "";
+                var endTime ="";
+                var area ="";
+                var name ="";
+                var pid="";
+                $("#tbody").find("tr").each(function(i,o){
+                    $(this).find("td").each(function(i){
+                        var clicked =  $(this).attr("clicked");
+                        if(clicked=='true'){
+                            courtId = $(this).attr("data-id");
+                            startTime= $(this).attr("data-starttime");
+                            endTime= $(this).attr("data-endtime");
+                            area= $(this).attr("data-area");
+                            name= $(this).attr("data-name");
+                            pid = $(this).attr("data-pid");
+                        }
+                    })
+                });
+                if(courtId==""){
+                    layer.alert("请选择一个时间");
+                    return;
+                }
+                var time = $("#date").val();
+
+                module.yud(pid,time,startTime,endTime,area,name);
+            });
         },
         initRecharge:function () {
             util.openLayer({
@@ -130,6 +159,59 @@ define([
                 }
             });
         },
+        yud:function (pid,time,startTime,endTime,area,name) {
+            util.openLayer({
+                area: '800px',
+                shade: [0.8, '#393D49'],
+                title: "预定",
+                type: 1,
+                content: $("#addWin2"),
+                btn: ['预定', '关闭'],
+                success: function (layero, index) {
+                    //获得选中的场地
+                    $("#areaSpan").text(area+"-"+name+"号场");
+                    $("#startTimeInput").val(startTime);
+                    $("#endTimeInput").val(endTime);
+                    $("#price").val("");
+                    $("#areaId").val(pid);
+                    $("#payType").val("");
+                    $("#person").val("");
+                    $("#phone").val("")
+                },
+                yes: function (layero, index) {
+                    var data = util.getTokenData();
+                    var payType = $("#payType").val();
+                    var phone = $("#phone2").val();
+                    data['areaId'] = $("#areaId").val();
+                    data['price']=$("#price").val();
+                    data['payType']=payType;
+                    data['phone']=phone;
+                    data['date']=$("#date").val();
+                    data['person']=$("#person").val();
+                    data['startTime']=$("#startTimeInput").val();
+                    data['endTime']=$("#endTimeInput").val();
+                    if (payType==""){
+                        layer.msg("请选择支付方式！");
+                        return;
+                    }
+                    console.log(data);
+                    if(!(/^1[3|4|5|7|8][0-9]{9}$/.test(phone))){
+                        layer.msg("手机号码有误，请重填");
+                        return;
+                    }
+                    util.post("bookCustomer/addOrder", data).then(function (d) {
+                        if (d.code == 200) {
+                            layer.closeAll();
+                        }
+                        layer.alert(d.message);
+                    });
+
+                },
+                cancel: function (index, layero) {
+                    layer.close(index);
+                }
+            });
+        },
         initTable:function (area,time) {
             var data = util.getTokenData();
             dojo.mixin(data, {"area": area,"time":time});
@@ -148,7 +230,7 @@ define([
                     }
                     th.append(thStr);
 
-                    var tbody = $("tbody");
+                    var tbody = $("#tbody");
                     tbody.empty();
                     var tbodyStr = "";
                     for(var i=0;i<list4FProduct.length;i++){
@@ -158,7 +240,8 @@ define([
                          tbodyStr+= " <td>"+tt+"-"+ett+"</td>" ;
                         for(var j=0;j<list4FArea.length;j++){
                             var index = i * list4FArea.length + j;
-                            tbodyStr +="<td data-id='"+listProduct[index].courtId+"' data-pid='"+listProduct[index].pid+"'></td>";
+                            tbodyStr +="<td class='tdContent' data-area='"+list4FArea[j].area+"' data-name='"+list4FArea[j].name+"'" +
+                                " data-startTime='"+tt+"' data-endTime ='"+ett+"' data-id='"+listProduct[index].courtId+"' data-pid='"+listProduct[index].pid+"'></td>";
 
                         }
                         tbodyStr+="</tr>";
@@ -169,11 +252,27 @@ define([
                             var pid = ($(this).attr("data-pid"));
                             for(var h = 0;h<customerList.length;h++){
                                 if(customerList[h].pid == pid){
-                                    $(this).attr("bgcolor","gray");
+                                    $(this).attr("bgcolor","#CB5A5E");
+                                    $(this).attr("fix","true");
                                     break;
                                 }
                             }
                         })
+                    });
+                    $(".tdContent").click(function(){
+                        var courtId = ($(this).attr("data-id"));
+                        var clicked =  $(this).attr("clicked");
+                        if(clicked == 'true'){
+                            $(this).attr("clicked", false).css("background-color", "");
+                        }else{
+                            $("#tbody").find("tr").each(function(i,o){
+                                $(this).find("td").each(function(i){
+                                    $(this).css("background-color", "");
+                                })
+                            });
+                            $(this).attr("clicked", true).css("background-color", "#CB5A5E");
+                        }
+
                     });
                 }
             });
